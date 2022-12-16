8d365a3c0c49fb16690d5b695c833b7957bf1670
Lai-Da <119024513+Lai-Da@users.noreply.github.com>
2022-12-16 12:04:47 +0800

    Add files via upload

---
+++ model/TaiwanNewsAnalysis.py
@@ -0,0 +1,288 @@
+from transformers import BertForSequenceClassification
+from torch.utils.data import DataLoader
+import os
+import pandas as pd
+import torch
+import pandas as pd
+import numpy as np
+import json
+from transformers import BertTokenizer, BertForSequenceClassification
+from IPython.display import clear_output
+from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
+
+df_train = pd.read_csv("./train_twnews.csv")
+
+# MAX_LENGTH = 500
+# df_train = df_train[(df_train.text.apply(lambda x : len(x)) > MAX_LENGTH)]
+
+df_train.to_csv("train_twnews.tsv", sep="\t", index=False)
+
+print("訓練樣本數：", len(df_train))
+df_train.head()
+
+df_test = pd.read_csv("./test_twnews.csv")
+df_test.to_csv("test_twnews.tsv", sep="\t", index=False)
+
+print("預測樣本數：", len(df_test))
+df_test.head()
+
+PRETRAINED_MODEL_NAME = "bert-base-chinese"
+tokenizer = BertTokenizer.from_pretrained(PRETRAINED_MODEL_NAME)
+
+
+class TWNewsDataset(Dataset):
+    # 讀取前處理後的 tsv 檔並初始化一些參數
+    def __init__(self, mode, tokenizer):
+        assert mode in ["train_twnews", "test_twnews"]  # 一般訓練你會需要 dev set
+        self.mode = mode
+        # 大數據你會需要用 iterator=True
+        self.df = pd.read_csv(mode + ".tsv", sep="\t").fillna("")
+        self.len = len(self.df)
+        self.label_map = {'Up': 0, 'Unchanged': 1, 'Down': 2}
+        self.tokenizer = tokenizer  # 我們將使用 BERT tokenizer
+
+    # 定義回傳一筆訓練 / 測試數據的函式
+    def __getitem__(self, idx):
+        if self.mode == "test":
+            text = self.df.iloc[idx, :].values
+            label_tensor = None
+        else:
+            text, label = self.df.iloc[idx, :].values
+            # 將 label 文字也轉換成索引方便轉換成 tensor
+            label_id = self.label_map[label]
+            label_tensor = torch.tensor(label_id)
+
+        # 建立一個句子的 BERT tokens 並加入分隔符號 [SEP]
+        word_pieces = ["[CLS]"]
+        tokens_a = self.tokenizer.tokenize(text)
+        word_pieces += tokens_a + ["[SEP]"]
+        len_a = len(word_pieces)
+
+        # 將整個 token 序列轉換成索引序列
+        ids = self.tokenizer.convert_tokens_to_ids(word_pieces)
+        tokens_tensor = torch.tensor(ids)
+
+        # 將包含 [SEP] 的 token 位置設為 1
+        segments_tensor = torch.tensor([1] * len_a, dtype=torch.long)
+
+        return (tokens_tensor, segments_tensor, label_tensor)
+
+    def __len__(self):
+        return self.len
+
+
+# 初始化一個專門讀取訓練樣本的 Dataset，使用中文 BERT 斷詞
+trainset = TWNewsDataset("train_twnews", tokenizer=tokenizer)
+
+# 選擇第一個樣本
+sample_idx = 0
+
+# 將原始文本拿出做比較
+text, label = trainset.df.iloc[sample_idx].values
+
+# 利用剛剛建立的 Dataset 取出轉換後的 id tensors
+tokens_tensor, segments_tensor, label_tensor = trainset[sample_idx]
+
+# 將 tokens_tensor 還原成文本
+tokens = tokenizer.convert_ids_to_tokens(tokens_tensor.tolist())
+combined_text = "".join(tokens)
+
+# 渲染前後差異，毫無反應就是個 print。可以直接看輸出結果
+print(f"""[原始文本]
+文本：{text}
+分類  ：{label}
+
+--------------------
+
+[Dataset 回傳的 tensors]
+tokens_tensor  ：{tokens_tensor}
+
+segments_tensor：{segments_tensor}
+
+label_tensor   ：{label_tensor}
+
+--------------------
+
+[還原 tokens_tensors]
+{combined_text}
+""")
+
+
+# 這個函式的輸入 `samples` 是一個 list，裡頭的每個 element 都是剛剛定義的 `FakeNewsDataset` 回傳的一個樣本，每個樣本都包含 3 tensors：
+# - tokens_tensor
+# - segments_tensor
+# - label_tensor
+# 它會對前兩個 tensors 作 zero padding，並產生前面說明過的 masks_tensors
+
+def create_mini_batch(samples):
+    tokens_tensors = [s[0] for s in samples]
+    segments_tensors = [s[1] for s in samples]
+
+    # 測試集有 labels
+    if samples[0][2] is not None:
+        label_ids = torch.stack([s[2] for s in samples])
+    else:
+        label_ids = None
+
+    # zero pad 到同一序列長度
+    tokens_tensors = pad_sequence(tokens_tensors, batch_first=True)
+    segments_tensors = pad_sequence(segments_tensors, batch_first=True)
+
+    # attention masks，將 tokens_tensors 裡頭不為 zero padding的位置設為 1 讓 BERT 只關注這些位置的 tokens
+    masks_tensors = torch.zeros(tokens_tensors.shape, dtype=torch.long)
+    masks_tensors = masks_tensors.masked_fill(tokens_tensors != 0, 1)
+
+    return tokens_tensors, segments_tensors, masks_tensors, label_ids
+
+
+# 初始化一個每次回傳 64 個訓練樣本的 DataLoader
+# 利用 `collate_fn` 將 list of samples 合併成一個 mini-batch 是關鍵
+BATCH_SIZE = 64
+trainloader = DataLoader(trainset, batch_size=BATCH_SIZE,
+                         collate_fn=create_mini_batch)
+
+data = next(iter(trainloader))
+
+tokens_tensors, segments_tensors, masks_tensors, label_ids = data
+
+print(f"""
+tokens_tensors.shape   = {tokens_tensors.shape} 
+{tokens_tensors}
+------------------------
+segments_tensors.shape = {segments_tensors.shape}
+{segments_tensors}
+------------------------
+masks_tensors.shape    = {masks_tensors.shape}
+{masks_tensors}
+------------------------
+label_ids.shape        = {label_ids.shape}
+{label_ids}
+""")
+
+# 載入一個可以做中文多分類任務的模型，n_class = 3
+
+PRETRAINED_MODEL_NAME = "bert-base-chinese"
+NUM_LABELS = 3
+
+model = BertForSequenceClassification.from_pretrained(
+    PRETRAINED_MODEL_NAME, num_labels=NUM_LABELS)
+
+clear_output()
+
+# high-level 顯示此模型裡的 modules
+print("""
+name            module
+----------------------""")
+for name, module in model.named_children():
+    if name == "bert":
+        for n, _ in module.named_children():
+            print(f"{name}:{n}")
+    else:
+        print("{:15} {}".format(name, module))
+
+
+def get_predictions(model, dataloader, compute_acc=False):
+    predictions = None
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        # 遍巡整個資料集
+        for data in dataloader:
+            # 將所有 tensors 移到 GPU 上
+            if next(model.parameters()).is_cuda:
+                data = [t.to("cuda:0") for t in data if t is not None]
+
+            # 別忘記前 3 個 tensors 分別為 tokens, segments 以及 masks
+            # 且強烈建議在將這些 tensors 丟入 `model` 時指定對應的參數名稱
+            tokens_tensors, segments_tensors, masks_tensors = data[:3]
+            outputs = model(input_ids=tokens_tensors,
+                            token_type_ids=segments_tensors, attention_mask=masks_tensors)
+
+            logits = outputs[0]
+            _, pred = torch.max(logits.data, 1)
+
+            # 用來計算訓練集的分類準確率
+            if compute_acc:
+                labels = data[3]
+                total += labels.size(0)
+                correct += (pred == labels).sum().item()
+
+            # 將當前 batch 記錄下來
+            if predictions is None:
+                predictions = pred
+            else:
+                predictions = torch.cat((predictions, pred))
+
+    if compute_acc:
+        acc = correct / total
+        return predictions, acc
+    return predictions
+
+
+# 讓模型跑在 GPU 上並取得訓練集的分類準確率
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("device:", device)
+model = model.to(device)
+_, acc = get_predictions(model, trainloader, compute_acc=True)
+print("classification acc:", acc)
+
+%% time
+# 訓練模式
+model.train()
+
+# 使用 Adam Optim 更新整個分類模型的參數
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+
+
+EPOCHS = 6  # 幸運數字
+for epoch in range(EPOCHS):
+
+    running_loss = 0.0
+    for data in trainloader:
+
+        tokens_tensors, segments_tensors, masks_tensors, labels = [
+            t.to(device) for t in data]
+
+        # 將參數梯度歸零
+        optimizer.zero_grad()
+
+        # forward pass
+        outputs = model(input_ids=tokens_tensors, token_type_ids=segments_tensors,
+                        attention_mask=masks_tensors, labels=labels)
+
+        loss = outputs[0]
+        # backward
+        loss.backward()
+        optimizer.step()
+
+        # 紀錄當前 batch loss
+        running_loss += loss.item()
+
+    # 計算分類準確率
+    _, acc = get_predictions(model, trainloader, compute_acc=True)
+
+    print('[epoch %d] loss: %.3f, acc: %.3f' %
+          (epoch + 1, running_loss, acc))
+
+%% time
+# 建立測試集。
+testset = TWNewsDataset("test_twnews", tokenizer=tokenizer)
+BATCH_SIZE = 64
+testloader = DataLoader(testset, batch_size=BATCH_SIZE,
+                        collate_fn=create_mini_batch)
+
+# 用分類模型預測測試集
+predictions = get_predictions(model, testloader)
+
+# 用來將預測的 label id 轉回 label 文字
+index_map = {v: k for k, v in testset.label_map.items()}
+
+# 生成檔案
+df = pd.DataFrame({'Category': predictions.tolist()})
+df['Category'] = df.Category.apply(lambda x: index_map[x])
+df_pred = pd.concat([testset.df.loc[:, ['title']],
+                    df.loc[:, 'Category']], axis=1)
+df_pred.to_csv('bert_1_prec_training_samples.csv', index=False)
+df_pred.head()
