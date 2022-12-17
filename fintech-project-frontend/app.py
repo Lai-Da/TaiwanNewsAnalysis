779b6f67ac7cc9e6e94bb97eaf25e1775ecf5def
ian94305 <ianianwuwu@gmail.com>
2022-12-17 18:25:26 +0800

    12/17 news page display

--- fintech-project-frontend/app.py
+++ fintech-project-frontend/app.py
@@ -12,18 +12,41 @@ db = pymysql.connect(
     port=3306,
     user='root',
     passwd = '',
-    database='login',
+    database='taiwannewsanalysis',
     charset='utf8mb4'
 )
 
 cursor = db.cursor()
+cursor.execute('SELECT * FROM `ctee_data1129_1201`')
+
+if cursor.rowcount > 0:
+    results = cursor.fetchall()
+
+    news_list = []
+    for i in results:
+        time = i[0]
+        title = i[1]
+        url = i[2]
+        text = i[3]
+        src = i[4]
+        news_list.append({"time": time, "title": title, "url": url, "text": text, "src": src})
+# cursor.close()
+
 
 @app.route('/',methods=['GET'])
 def home():
     return render_template("./home/index.html")
+
 @app.route('/profolio',methods=['GET'])
 def profolio():
     return render_template("./home/profolio.html")
+
+@app.route('/news',methods=['GET', "POST"])
+def news():
+    if request.method == 'GET':
+        return render_template('./home/news.html', news_list=news_list)
+
+
 
 @app.route('/log_in', methods=['GET', 'POST'])
 def log_in():
@@ -35,43 +58,6 @@ def log_in():
             return redirect(url_for('main_page')) #return render_template('main_page.html')
     return render_template('./home/login.html', error=error)
 
-# @app.route('/news',methods=['GET', "POST"])
-# def news():
-#     if request.method == 'GET':
-#         return render_template('news.html', product_list=product_list)
-
-#     if request.method == 'POST':
-#         cat_list = ['', 'OZWEEGO', 'Superstar', 'Stan', 'Smith', 'others', 'Retropy', 'Forum', 'Campus', 'Puffylette']
-
-#         cursor = db.cursor()
-#         cllection_insert = """INSERT INTO `collection` VALUES (%s);"""
-#         # p_id = request.values.get('p_id') # 用 form 的方法
-#         # cursor.execute(cllection_insert, (p_id))
-#         try:
-#             cursor.execute(cllection_insert, (request.json["id"])) # 用 js 的方法
-#             db.commit()
-#         except:
-#             print('selection process')
-
-#         selection_sql = f"""SELECT `category`.`id`, `category`.`name`, `productid_imgurl`.`imgURL` FROM `category` 
-#                             INNER JOIN `productid_imgurl` ON `category`.`id` = `productid_imgurl`.`id`
-#                             WHERE `category`.`category` = '{request.values.get('c_id')}'"""
-#         cursor.execute(selection_sql)
-
-#         product_list1 = []
-#         if cursor.rowcount > 0:
-#             results = cursor.fetchall()
-
-#             product_list1 = []
-#             for i in results:
-#                 ids = i[0]
-#                 # names = " ".join(re.findall("\w+\s+", i[2]))
-#                 names = i[1]
-#                 url = i[2]
-#                 product_list1.append({"id": ids, "name": names, "url": url})
-
-#         cursor.close()
-#         return render_template('product_page.html', product_list=product_list1)
 @app.route('/register',methods=['GET'])
 def register():
     return render_template("./home/register.html")
@@ -88,7 +74,7 @@ def registration():
             res["info"] = "註冊成功"
             res["Username"] = request.json["Username"]
             res["Password"] = request.json["Password"]
-            return render_template("./admin/index.html")
+            # return render_template("./admin/index.html")
         else:
             res["info"] = "新增失敗"
         
@@ -147,5 +133,6 @@ def registration():
 #         res["info"] = f"SQL 執行失敗:{e}"
     
 #     return jsonify(res)
-app.debug=True
-app.run()
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
